df1752ba3fa4ba4fec6b56fe4c903f1d44c52774
Stefan Turner <turner.stefan281@gmail.com>
2022-12-18 15:54:14 +0100

    add desktop and dark mode style

--- src/js/components/Details.js
+++ src/js/components/Details.js
@@ -10,48 +10,49 @@ export const Details = async (data) => {
           let container = document.querySelector(".details__dynamic");
           container.innerHTML = "";
           let details = document.createElement("div");
-          details.className = "details__dynamicContent";
+          details.className = "details__wrapper";
           details.innerHTML = `
-                <img src=${country.flag} alt="Country Flag" />
-                <h2>${country.name}</h2>
-                <div class="details__infosContainer">
-                    <div class="details__infos--primary">
-                        <p><span>Native Name:</span> ${country.nativeName}</p>
-                        <p><span>Population:</span> ${country.population}</p>
-                        <p><span>Region:</span> ${country.region}</p>
-                        <p><span>Sub Region:</span> ${country.subregion}</p>
-                        <p><span>Capital:</span> ${country.capital}</p>
-                    </div>
-                    <div class="details__infos--secondary">
-                        <p><span>Top Level Domain:</span> ${
-                          country.topLevelDomain
-                        }</p>
-                        <p><span>Currencies:</span> ${country.currencies.map(
-                          (currency) => currency.name
-                        )}</p>
-                        <p><span>Languages:</span> ${country.languages.map(
-                          (language) => language.name
-                        )}</p>
-                    </div>
+            <img src=${country.flag} class="details__img" alt="Country Flag" />
+            <div class="details__text">
+              <h2>${country.name}</h2>
+              <div class="details__infos">
+                <div class="details__infos--primary">
+                  <p><span>Native Name:</span> ${country.nativeName}</p>
+                  <p><span>Population:</span> ${country.population}</p>
+                  <p><span>Region:</span> ${country.region}</p>
+                  <p><span>Sub Region:</span> ${country.subregion}</p>
+                  <p><span>Capital:</span> ${country.capital}</p>
                 </div>
-                <div class="details__bordersContainer">
-                    <h3>Border Countries:</h3>
-                    <div class="details__borders">
-                    
-                          ${
-                            country.borders
-                              ? country.borders
-                                  .map((border) => {
-                                    return `<button class="details__button">${convertCountryCode(
-                                      border
-                                    )}</button>`;
-                                  })
-                                  .join(" ")
-                              : "No Bordering country"
-                          }
-                    </div>            
+                <div class="details__infos--secondary">
+                  <p><span>Top Level Domain:</span> ${
+                    country.topLevelDomain
+                  }</p>
+                  <p><span>Currencies:</span> ${country.currencies.map(
+                    (currency) => currency.name
+                  )}</p>
+                  <p><span>Languages:</span> ${country.languages.map(
+                    (language) => language.name
+                  )}</p>
                 </div>
-              `;
+              </div>
+              <div class="details__borders">
+                <h3>Border Countries:</h3>
+                <div class="details__borderButtons">
+                ${
+                  country.borders
+                    ? country.borders
+                        .map((border) => {
+                          return `<button class="details__button">${convertCountryCode(
+                            border
+                          )}</button>`;
+                        })
+                        .join(" ")
+                    : "No Bordering country"
+                }
+                </div>
+              </div>
+            </div>
+            `;
 
           container.append(details);
         }
